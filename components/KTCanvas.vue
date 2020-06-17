2fa52490d84714103add3ecfe7d5a2ae63cf8eb1
3sdd <56486103+3sdd@users.noreply.github.com>
2020-06-17 23:09:27 +0900

    ボタンを押すと図形をcanvasに表示するようにした

--- components/KTCanvas.vue
+++ components/KTCanvas.vue
@@ -12,22 +12,22 @@
                             <v-container>
                                 <v-row>
                                     <div>
-                                        <v-btn color="primary">
+                                        <v-btn color="primary" @click="createLine">
                                             <img src="~/assets/icon-line.svg" width="32" height="32">
                                         </v-btn>
                                     </div>
                                     <div>
-                                        <v-btn color="primary">
+                                        <v-btn color="primary" @click="createCircle">
                                             <img src="~/assets/icon-circle.svg" width="32" height="32">
                                         </v-btn>
                                     </div>
                                     <div>
-                                        <v-btn color="primary">
+                                        <v-btn color="primary" @click="createRectangle">
                                             <img src="~/assets/icon-rectangle.svg" width="32" height="32">
                                         </v-btn>
                                     </div>
                                     <div>
-                                        <v-btn color="primary">
+                                        <v-btn color="primary" @click="createArrow">
                                             <img src="~/assets/icon-arrow.svg" width="32" height="32">
                                         </v-btn>
                                     </div>
@@ -64,6 +64,11 @@
 import {fabric} from "fabric"
 
 
+//stroke を変更した時にサイズが変わる問題
+//https://stackoverflow.com/questions/49005241/maintain-strokewidth-while-scaling-in-fabric-js
+//lineのfillが効かない
+//https://stackoverflow.com/questions/18835580/how-to-draw-a-line-on-a-canvas-using-fabric-js
+
 export default {
     mounted(){
 
@@ -76,29 +81,6 @@ export default {
             // selectionColor:"red",
             // selectionLineWidth:2,
         });
-        const rect=new fabric.Rect({
-            left:100,
-            top:100,
-            fill:"red",
-            width:20,
-            height:20,
-            angle:45
-        })
-        rect.set({
-            strokeWidth:5,
-            stroke:"white"
-        })
-        console.log(rect.getAngleInRadians())
-        this.canvas.add(rect);
-
-        const circle=new fabric.Circle({
-            radius:20,
-            fill:"red",
-            left:200,
-            top:200,
-        })
-        console.log(circle.getAngleInRadians())
-        this.canvas.add(circle);
 
         // console.log(this.canvas.item(0));
         // console.log(this.canvas.getObjects())
@@ -139,6 +121,76 @@ export default {
     data(){
         return {
             canvas:null
+        }
+    },
+    methods:{
+        createLine(){
+            const line=new fabric.Line([50,100,200,100],{
+                left:100,
+                top:100,
+                stroke:"red",
+            });
+            this.canvas.add(line);
+            this.canvas.renderAll();
+        },
+        createCircle(){
+            const circle=new fabric.Circle({
+                left:100,
+                top:100,
+                fill:"rgba(0,0,0,0)",
+                stroke:"red",
+                strokeWidth:2,
+                strokeUniform:true,
+                radius:40
+            });
+            this.canvas.add(circle);
+            this.canvas.renderAll();
+        },
+        createRectangle(){
+            const rectangle=new fabric.Rect({
+                left:100,
+                top:100,
+                fill:"rgba(0,0,0,0)",
+                stroke:"red",
+                strokeWidth:2,
+                strokeUniform:true,
+                width:200,
+                height:50,
+            });
+            
+            this.canvas.add(rectangle);
+            this.canvas.renderAll();
+        },
+        createArrow(){
+
+            const arrow = new fabric.Path('M121.32,0L44.58,0C36.67,0,29.5,3.22,24.31,8.41\
+            c-5.19,5.19-8.41,12.37-8.41,20.28c0,15.82,12.87,28.69,28.69,28.69c0,0,4.4,\
+            0,7.48,0C36.66,72.78,8.4,101.04,8.4,101.04C2.98,106.45,0,113.66,0,121.32\
+            c0,7.66,2.98,14.87,8.4,20.29l0,0c5.42,5.42,12.62,8.4,20.28,8.4c7.66,0,14.87\
+            -2.98,20.29-8.4c0,0,28.26-28.25,43.66-43.66c0,3.08,0,7.48,0,7.48c0,15.82,\
+            12.87,28.69,28.69,28.69c7.66,0,14.87-2.99,20.29-8.4c5.42-5.42,8.4-12.62,8.4\
+            -20.28l0-76.74c0-7.66-2.98-14.87-8.4-20.29C136.19,2.98,128.98,0,121.32,0z');
+            arrow.set({
+                left:100,
+                top:100,
+                width:150,
+                height:150,
+                fill:"red"
+            }).scale(0.5);
+            this.canvas.add(arrow);
+            this.canvas.renderAll();
+
+            // const url="http://fabricjs.com/assets/1.svg"
+            // fabric.loadSVGFromURL(url, function(objects) { 
+            //         var group = new fabric.PathGroup(objects, { 
+            //         left: 165, 
+            //         top: 100, 
+            //         width: 295, 
+            //         height: 211 
+            //         }); 
+            //         canvas.add(group); 
+            //         canvas.renderAll(); 
+            //         }); 
         }
     }
 }
