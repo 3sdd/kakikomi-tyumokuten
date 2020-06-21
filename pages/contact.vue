ba11731493cc5490bd4fd2a53c79b8c8a8be73e8
3sdd <56486103+3sdd@users.noreply.github.com>
2020-06-21 14:51:48 +0900

    お問合せページ作成

---
+++ pages/contact.vue
@@ -0,0 +1,6 @@
+<template>
+    <v-layout justify-center="">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScW_v64ChhHeIhMj5oZHNRCJcBqirIdidAmih3h7odyXBFGDg/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+
+    </v-layout>
+</template>
