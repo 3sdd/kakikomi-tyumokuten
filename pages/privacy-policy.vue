a9d98a5762fdf6b5849973c6f8457c7387e78817
3sdd <56486103+3sdd@users.noreply.github.com>
2020-06-21 10:41:50 +0900

    privacy-policyのページ作成

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-layout justify-center="">
+        <v-card class="pa-4" width="700">
+            <v-card-title>
+                プライバシー・ポリシー
+            </v-card-title>
+            <div>
+                <h2>適用範囲</h2>
+                <p>
+                    ユーザーの個人情報の取扱いについて，以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます）を定めます。 
+                    本プライバシーポリシーは「書き込み注目点のサイト」(本サイト)および、
+                    本サイトで提供されている「書き込み注目点のサービス」(以下「本サービス」)に適用されます。
+                </p>
+            </div>
+            <div>
+                <h2>個人情報</h2>
+                <p>
+                    本サイトでは、本サービス向上や新たなサービス開発のため、個人情報を収集することがあります。
+                    収集された個人情報は、適切な管理、保護に努めます。
+                </p>
+            </div>
+            <div>
+                <h2>アクセス解析ツールについて</h2>
+                <p>
+                    本サイトでは、アクセス状況の把握のため、Googleによるアクセス解析ツール「Google Analytics」を使用しています。 
+                    Google Analyticsでは、Cookieを用いて利用者の情報を匿名で収集しています。
+                    これらの機能の停止につきましては、お使いのブラウザーの設定でCookieを無効にすることで可能です。
+                    これらの詳細は、
+                    <a href="https://support.google.com/analytics/answer/4597324">
+                        https://support.google.com/analytics/answer/4597324
+                    </a>をご確認ください。
+                </p>
+            </div>
+            <div>
+                <h2>免責</h2>
+                <p>
+                    本サービスを使用して生じた損害等の一切の責任は負わないものとします。
+                </p>
+            </div>
+            <div>
+                <h2>プライバシーポリシーの変更</h2>
+                <p>
+                    本ポリシーは、事前にユーザーに通知することなく、変更することがあります。
+                </p>
+            </div>
+        </v-card>
+    </v-layout>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+h1{
+    font-size: 1.5rem;
+}
+</style>
