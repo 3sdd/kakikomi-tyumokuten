96929718dafd11dcd28d40648f1283e3c17adeed
3sdd <56486103+3sdd@users.noreply.github.com>
2020-07-07 21:06:38 +0900

    お問合せフォームをレスポンシブに修正

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
     <v-layout justify-center="">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScW_v64ChhHeIhMj5oZHNRCJcBqirIdidAmih3h7odyXBFGDg/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScW_v64ChhHeIhMj5oZHNRCJcBqirIdidAmih3h7odyXBFGDg/viewform?embedded=true" width="100%" height="1200" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
 
     </v-layout>
 </template>
